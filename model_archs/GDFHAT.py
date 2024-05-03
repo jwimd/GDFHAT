efed692ef7208f616453fe281ba2f251887bd2dc
jwimd <chenjiewei@zju.edu.cn>
2024-05-03 15:23:34 +0800

    GDFHAB

--- model_archs/GDFHAT.py
+++ model_archs/GDFHAT.py
@@ -482,6 +482,133 @@ class HAB(nn.Module):
         x = x + self.drop_path(self.mlp(self.norm2(x)))
 
         return x
+    
+class GDFHAB(nn.Module):
+    r""" Modified Hybrid Attention Block: TTSA + WSA.
+
+    Args:
+        dim (int): Number of input channels.
+        input_resolution (tuple[int]): Input resolution.
+        num_heads (int): Number of attention heads.
+        window_size (int): Window size.
+        shift_size (int): Shift size for SW-MSA.
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set.
+        drop (float, optional): Dropout rate. Default: 0.0
+        attn_drop (float, optional): Attention dropout rate. Default: 0.0
+        drop_path (float, optional): Stochastic depth rate. Default: 0.0
+        act_layer (nn.Module, optional): Activation layer. Default: nn.GELU
+        norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
+    """
+
+    def __init__(self,
+                 dim,
+                 input_resolution,
+                 num_heads,
+                 window_size=7,
+                 shift_size=0,
+                 compress_ratio=3,
+                 squeeze_factor=30,
+                 conv_scale=0.01,
+                 mlp_ratio=4.,
+                 qkv_bias=True,
+                 qk_scale=None,
+                 drop=0.,
+                 attn_drop=0.,
+                 drop_path=0.,
+                 act_layer=nn.GELU,
+                 norm_layer=nn.LayerNorm,
+                 ffn_expansion_factor = 2.66,
+                 bias = False):
+        super().__init__()
+        self.dim = dim
+        self.input_resolution = input_resolution
+        self.num_heads = num_heads
+        self.window_size = window_size
+        self.shift_size = shift_size
+        self.mlp_ratio = mlp_ratio
+        if min(self.input_resolution) <= self.window_size:
+            # if window size is larger than input resolution, we don't partition windows
+            self.shift_size = 0
+            self.window_size = min(self.input_resolution)
+        assert 0 <= self.shift_size < self.window_size, 'shift_size must in 0-window_size'
+
+        self.norm1 = norm_layer(dim)
+        self.attn = WindowAttention(
+            dim,
+            window_size=to_2tuple(self.window_size),
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            attn_drop=attn_drop,
+            proj_drop=drop)
+
+        self.conv_scale = conv_scale
+        # self.conv_block = CAB(num_feat=dim, compress_ratio=compress_ratio, squeeze_factor=squeeze_factor)
+        self.conv_block = TTSA(dim, num_heads, qkv_bias)  # replace CA with TTSA
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        #mlp_hidden_dim = int(dim * mlp_ratio)
+        #self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        
+        self.GDFN = FeedForward(self.dim, ffn_expansion_factor, bias=bias)
+
+    def forward(self, x, x_size, rpi_sa, attn_mask):
+        h, w = x_size
+        b, _, c = x.shape
+        # assert seq_len == h * w, "input feature has wrong size"
+
+        shortcut = x
+        x = self.norm1(x)
+        x = x.view(b, h, w, c)
+
+        # Conv_X
+        conv_x = self.conv_block(x.permute(0, 3, 1, 2))
+        conv_x = conv_x.permute(0, 2, 3, 1).contiguous().view(b, h * w, c)
+
+        # cyclic shift
+        if self.shift_size > 0:
+            shifted_x = torch.roll(x, shifts=(-self.shift_size, -self.shift_size), dims=(1, 2))
+            attn_mask = attn_mask
+        else:
+            shifted_x = x
+            attn_mask = None
+
+        # partition windows
+        x_windows = window_partition(shifted_x, self.window_size)  # nw*b, window_size, window_size, c
+        x_windows = x_windows.view(-1, self.window_size * self.window_size, c)  # nw*b, window_size*window_size, c
+
+        # W-MSA/SW-MSA (to be compatible for testing on images whose shapes are the multiple of window size
+        attn_windows = self.attn(x_windows, rpi=rpi_sa, mask=attn_mask)
+
+        # merge windows
+        attn_windows = attn_windows.view(-1, self.window_size, self.window_size, c)
+        shifted_x = window_reverse(attn_windows, self.window_size, h, w)  # b h' w' c
+
+        # reverse cyclic shift
+        if self.shift_size > 0:
+            attn_x = torch.roll(shifted_x, shifts=(self.shift_size, self.shift_size), dims=(1, 2))
+        else:
+            attn_x = shifted_x
+        # print(attn_x.size())
+
+        attn_x = attn_x.view(b, h * w, c)
+        # FFN
+        x = shortcut + self.drop_path(attn_x) + conv_x * self.conv_scale
+        
+        #layer norm
+        self.norm2(x)
+        # reshape b h*w c -> b c h w
+        x = x.reshape(b, c, h, w)
+        #GDFN
+        x = self.drop_path(self.GDFN(x))
+        # reshape b c h w -> b h*w c 
+        x = x.reshape(b, h * w, c)
+        
+
+        return x
 
 
 class PatchMerging(nn.Module):
@@ -717,8 +844,7 @@ class GDFOCAB(nn.Module):
         #GDFN
         x = self.GDFN(x)
         # reshape b c h w -> b h*w c 
-        x = x.view(b, h * w, self.dim)
-        
+        x = x.view(b, h * w, c)
         return x
 
 class AttenBlocks(nn.Module):
@@ -772,7 +898,7 @@ class AttenBlocks(nn.Module):
 
         # build blocks
         self.blocks = nn.ModuleList([
-            HAB(
+            GDFHAB(
                 dim=dim,
                 input_resolution=input_resolution,
                 num_heads=num_heads,
@@ -1287,6 +1413,6 @@ if __name__ == '__main__':
     print("Param: {} M".format(params/1e6))
     print("FLOPs: {} G".format(flops/1e9))
     
-    output = model(input)
+    #output = model(input)
     #print(output.size())
 
